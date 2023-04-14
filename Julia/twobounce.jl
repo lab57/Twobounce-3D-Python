787a7be79d58edef041efc85f3d1cec17cc5918e
Luc Barrett <luc.barrett57@gmail.com>
2023-04-14 17:45:37 -0400

    julia stuff

--- Julia/twobounce.jl
+++ Julia/twobounce.jl
@@ -3,8 +3,26 @@ using FileIO
 function objLoad(f::String)
     file = open(f)
     lines = readlines(file)
-    for 
+
+    verticies = []
+    objects = []
+    triangles = []
+    normals = []
+    texture_verticies = []
+    curObject:string = nothing
+    for line in lines
+        if (line[1] == 'o')
+            println(line)
+
+        elseif (line[1] == 'v' && line[2] == ' ')
+            println("push")
+            spt = split(line, " ")
+            push!(verticies, Vector{Float64}(map(Float64, spt)))
+        end
+
+    end
+    return verticies
 
 end
-
-objLoad("./FourCubes.obj")
+println("Object load")
+println(objLoad("./FourCubes.obj"))
